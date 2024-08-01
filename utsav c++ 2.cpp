2ea5ea8eaca0637b1801da73a2b49270a6a86c21
aervadiyautsav <ervadiyautsav@gmail.com>
2024-08-01 07:37:07 +0530

    Add files via upload

---
+++ utsav c++ 2.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>  
+  
+int main()  
+{  
+    float  per;  
+  
+    printf("70");  
+      
+  
+    
+  
+    if(per >= 60)  
+    {  
+        printf("Grade A\n");  
+    }  
+    else  
+    {  
+        if(per >= 50)  
+        {  
+            printf("Grade B\n");  
+        }  
+        else  
+        {  
+            if(per >= 40)  
+            {  
+                printf("Grade C\n");  
+            }  
+            else  
+            {  
+                printf("Failed!\n");  
+            }  
+        }  
+    }  
+  
+    
+}  
